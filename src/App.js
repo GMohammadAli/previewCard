30cdbed6cf966fec6c4a9d7aef2ed16edfe2586d
GMohammadAli <gulammohammadali@gmail.com>
2022-07-05 23:06:21 +0530

    Styles Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { 
+  Grid,
+  Card,
+  CardMedia,
+  Typography,
+  CardContent,
+  CardActions,
+  Button
+} from "@mui/material"
+import Image from "./images/image-product-desktop.jpg";
+import {ShoppingCart} from "@mui/icons-material"
+import "./App.css"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Card className="card-container">
+        <Grid container>
+          <Grid item xs={12} sm={6} md={6}>
+            <CardMedia
+              component="img"
+              image={Image}
+              alt="product image"
+              className="img-size"
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={6} className="col-size" zeroMinWidth>
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                <span>Perfume</span>
+              </Typography>
+              <Typography gutterBottom variant="h4" component="div">
+                <p>
+                  Gabrielle
+                  <br /> Essence Eau De Parfum
+                </p>
+              </Typography>
+              <Typography
+                variant="h6"
+                color="text.secondary"
+                component="div"
+                className="paragraph-card"
+              >
+                A floral,solar and voluptuous interpretation composed by Olivier
+                Polge,Permufer-creator for the house of CHANEL
+              </Typography>
+              <Typography gutterBottom className="prices-card">
+                <b>$149.99</b>
+                <del style={{ fontFamily: "Arial", marginLeft: "5%" }}>
+                  $169.99
+                </del>
+              </Typography>
+            </CardContent>
+            <CardActions className="action-card">
+              <Button className="button-card" variant="container" size="large">
+                <ShoppingCart />
+                Add to Cart
+              </Button>
+            </CardActions>
+          </Grid>
+        </Grid>
+      </Card>
     </div>
   );
 }
